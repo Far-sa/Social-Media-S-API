2017fff186a04378d044264c77fe95439c3e6fbc
Far-sa <fmohtasham82@gmail.com>
2022-08-29 22:52:00 +0300

    Create Users/Routes

--- Server.js
+++ Server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const helmet = require('helmet')
 
 const connectDB = require('./config/db')
 const { errorHandler } = require('./middleware/errorHandler')
@@ -15,17 +17,20 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(setHeaders)
+app.use(helmet())
+app.use(morgan('common'))
 
 //? Routes
 app.use('/', (req, res) => {
   res.send('Hello World')
 })
+app.use('/users', require('./routes/users'))
 
 //? ERR Handler
 app.use(errorHandler)
 
 //?Lunch App
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Sever is Running on ${PORT}`)
