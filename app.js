a1e143d1c97bd8bb91e970890847e51d34a5568c
Far-sa <fmohtasham82@gmail.com>
2022-08-29 22:25:24 +0300

    Done with basic functional structure

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express')
+const dotenv = require('dotenv')
 
+const connectDB = require('./config/db')
+
+//? Configuration
+dotenv.config()
+connectDB()
 
 const app = express()
 
-app.listen(3000, () => {
-  console.log('Server is running on Port 3000')
+//? Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+//? Routes
+app.use('/', (req, res) => {
+  res.send('Hello World')
+})
+
+//? ERR Handler
+
+//?Lunch App
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+  console.log(`Sever is Running on ${PORT}`)
 })
