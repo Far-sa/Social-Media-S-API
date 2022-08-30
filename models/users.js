d6e4fb38dac8c2aa1981554039723aa89acbf62c
Far-sa <fmohtasham82@gmail.com>
2022-08-30 16:36:14 +0300

    update User Model

--- models/users.js
+++ models/users.js
@@ -9,7 +9,11 @@ const UserSchema = new mongoose.Schema(
     cover_picture: { type: String, default: '' },
     followers: { type: [mongoose.Types.ObjectId], default: [] },
     followings: { type: [mongoose.Types.ObjectId], default: [] },
-    isAdmin: { type: Boolean, defaults: false }
+    isAdmin: { type: Boolean, defaults: false },
+    desc: { type: String, defaults: '', max: 50 },
+    city: { type: String, max: 50 },
+    from: { type: String, max: 50 },
+    relationship: { type: Number, enum: [1, 2, 3] }
   },
   { timestamps: true }
 )
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,27 @@ const { Router } = require('express')
 
 const router = new Router()
 
+//@ Description Get a User
+//@ Route GET/api/users/:id
 router.get('/', (req, res) => {
+  res.send('Users Info..')
+})
+
+//@ Description Follow a User
+//@ Route GET/api/users/:id
+router.get('/', (req, res) => {
+  res.send('Users Info..')
+})
+
+//@ Description update a user
+//@ Route PUT/api/users/update:id
+router.put('/', (req, res) => {
+  res.send('Users Info..')
+})
+
+//@ Description delete a user
+//@ Route DELETE/api/users/update:id
+router.delete('/', (req, res) => {
   res.send('Users Info..')
 })
 
